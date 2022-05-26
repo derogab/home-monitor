e047096e1acc12eb56442a6020beeedf9de501e2
Federica Di Lauro <federicadilauro1998@gmail.com>
2022-05-26 15:34:21 +0200

    add influxdb logging to master

--- web/influxdb_helper.py
+++ web/influxdb_helper.py
@@ -1,21 +1,19 @@
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-def main():
+
+def getInfluxClient():
     influxClient = InfluxDBClient.from_config_file("influx_config.ini")
+    return influxClient
+
+
+def createBucketInflux(influxClient, bucket_name):
     buckets_api = influxClient.buckets_api()
-
-    bucket_name = "test-py-bucket"
-
     if buckets_api.find_bucket_by_name(bucket_name) is None:
         buckets_api.create_bucket(bucket_name=bucket_name)
+    return
 
+def writeDataToInflux(influxClient, bucket_name, mac, type, value):
     write_api = influxClient.write_api(write_options=SYNCHRONOUS)
-    query_api = influxClient.query_api()
-    p = Point("my_measurement").tag("location", "Prague").field("temperature", 28.3)
+    p = Point(mac).field(type, value)
     write_api.write(bucket=bucket_name, record=p)
-
-
-
-if __name__ == "__main__":
-    main()
