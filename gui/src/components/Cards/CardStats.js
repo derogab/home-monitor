13f73208a256306ddfccfc0fe8391a74a4c81b3b
Gabriele De Rosa <derosagabriele@outlook.it>
2022-05-27 17:12:27 +0200

    Remove useless charts

--- gui/src/components/Cards/CardStats.js
+++ gui/src/components/Cards/CardStats.js
@@ -4,10 +4,6 @@ import PropTypes from "prop-types";
 export default function CardStats({
   statSubtitle,
   statTitle,
-  statArrow,
-  statPercent,
-  statPercentColor,
-  statDescripiron,
   statIconName,
   statIconColor,
 }) {
@@ -35,21 +31,6 @@ export default function CardStats({
               </div>
             </div>
           </div>
-          <p className="text-sm text-blueGray-400 mt-4">
-            <span className={statPercentColor + " mr-2"}>
-              <i
-                className={
-                  statArrow === "up"
-                    ? "fas fa-arrow-up"
-                    : statArrow === "down"
-                    ? "fas fa-arrow-down"
-                    : ""
-                }
-              ></i>{" "}
-              {statPercent}%
-            </span>
-            <span className="whitespace-nowrap">{statDescripiron}</span>
-          </p>
         </div>
       </div>
     </>
@@ -59,10 +40,6 @@ export default function CardStats({
 CardStats.defaultProps = {
   statSubtitle: "Traffic",
   statTitle: "350,897",
-  statArrow: "up",
-  statPercent: "3.48",
-  statPercentColor: "text-emerald-500",
-  statDescripiron: "Since last month",
   statIconName: "far fa-chart-bar",
   statIconColor: "bg-red-500",
 };
@@ -70,12 +47,8 @@ CardStats.defaultProps = {
 CardStats.propTypes = {
   statSubtitle: PropTypes.string,
   statTitle: PropTypes.string,
-  statArrow: PropTypes.oneOf(["up", "down"]),
-  statPercent: PropTypes.string,
   // can be any of the text color utilities
   // from tailwindcss
-  statPercentColor: PropTypes.string,
-  statDescripiron: PropTypes.string,
   statIconName: PropTypes.string,
   // can be any of the background color utilities
   // from tailwindcss
