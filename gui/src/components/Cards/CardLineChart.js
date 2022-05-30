08908932568c56d2aae55dbd28eaae2c14d479a6
Gabriele De Rosa <derosagabriele@outlook.it>
2022-05-30 02:50:37 +0200

    Hide graph

--- gui/src/components/Cards/CardLineChart.js
+++ gui/src/components/Cards/CardLineChart.js
@@ -108,14 +108,14 @@ export default function CardLineChart() {
   }, []);
   return (
     <>
-      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-blueGray-700">
+      <div className="display-none relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-blueGray-700">
         <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
           <div className="flex flex-wrap items-center">
             <div className="relative w-full max-w-full flex-grow flex-1">
               <h6 className="uppercase text-blueGray-100 mb-1 text-xs font-semibold">
                 Overview
               </h6>
-              <h2 className="text-white text-xl font-semibold">Sales value</h2>
+              <h2 className="text-white text-xl font-semibold">Temperature</h2>
             </div>
           </div>
         </div>
