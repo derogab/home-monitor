21620cc6f2549275cc137ed5d9f1b945e811af58
Gabriele De Rosa <derosagabriele@outlook.it>
2022-05-30 04:10:36 +0200

    Update Status

--- gui/src/components/Cards/CardStats.js
+++ gui/src/components/Cards/CardStats.js
@@ -1,19 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const CardStats = ({
   statId,
   statTitle,
-  statData,
   statIconName,
   statIconColor,
 }) => {
-  // Init stateful
-  const [value, setValue] = useState(1);
+  const [statDataValue, setStatDataValue] = useState(false);
+  
+  // Update data
+  useEffect(() => {
+    setTimeout(() => {
+      
+      fetch('http://localhost:3001/' + statId, {
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+      .then((response) => {
+        // Empty array if no data
+        if (!response.ok) return;
+        // Parse data
+        return response.json();
+      })
+      .then((data) => {
+        // Check if there are data
+        if (!data) return;
+        // Get value
+        const value = data.value || [];
+        // Update state
+        setStatDataValue(value);
+        // Log
+        console.log('Update value of ' + statId + ': ' + value);
+      })
+      .catch(function (err) {
+        console.log("Unable to fetch - ", err);
+      });
+
+    }, 5000);
+  }, [statId]);
+
   // Print data
   return (
     <>
-      <div id={statId} className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg fixed-min-height" onClick={() => setValue(!value)}>
+      <div id={statId} className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg fixed-min-height">
         <div className="flex-auto p-4">
           <div className="flex flex-wrap">
             <div className="relative w-full pr-4 max-w-full flex-grow flex-1">
@@ -21,7 +54,7 @@ const CardStats = ({
                 {statTitle}
               </h5>
               <span className="font-semibold text-xl text-blueGray-700">
-                {statData}
+                {statDataValue ? "YES" : "NO"}
               </span>
             </div>
             <div className="relative w-auto pl-4 flex-initial">
@@ -42,15 +75,15 @@ const CardStats = ({
 };
 
 CardStats.defaultProps = {
+  statId: "light",
   statTitle: "Example",
-  statData: "1234",
   statIconName: "far fa-chart-bar",
   statIconColor: "bg-red-500",
 };
 
 CardStats.propTypes = {
+  statId: PropTypes.string,
   statTitle: PropTypes.string,
-  statData: PropTypes.string,
   // can be any of the text color utilities
   // from tailwindcss
   statIconName: PropTypes.string,
