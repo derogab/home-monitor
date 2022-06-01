cec5bb96441af317aefc67b65d1b6bba8cda8f16
Gabriele De Rosa <derosagabriele@outlook.it>
2022-06-01 12:34:12 +0200

    Transfer secrets to ENVs

---
+++ web/secrets.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+
+# Take environment variables from .env
+load_dotenv()
+
+# Load all the useful variables
+
+IOT_HOST = os.environ.get('IOT_HOST')
+
+MQTT_BROKERIP = os.environ.get('MQTT_BROKERIP')
+MQTT_ID = os.environ.get('MQTT_ID')
+MQTT_PW = os.environ.get('MQTT_PW')
+
+MYSQL_ID = os.environ.get('MYSQL_ID')
+MYSQL_PW = os.environ.get('MYSQL_PW')
