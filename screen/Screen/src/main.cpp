1df1fa1244d5aa5faebc946a28f2e141729e8046
Federica Di Lauro <federicadilauro1998@gmail.com>
2022-06-14 13:22:45 +0200

    remove dec button and alarm button

--- screen/Screen/src/main.cpp
+++ screen/Screen/src/main.cpp
@@ -16,8 +16,6 @@
 #define DISPLAY_REFRESH_RATE 5000
 
 #define incPin D3
-#define decPin D4
-#define ALARM_BUTTON D5
 #define DEVICE_BUTTON D6
 #define BUTTON_DEBOUNCE_DELAY 200 // button debounce time in ms
 #define USER_DELAY 30000
@@ -51,25 +49,17 @@ volatile byte displayMode = 0;
 volatile int device_index = 0;
 
 volatile unsigned long last_interrupt_inc = 0;
-volatile unsigned long last_interrupt_dec = 0;
-volatile unsigned long last_interrupt_alarm = 0;
 volatile unsigned long last_interrupt_devices_display = 0;
 volatile unsigned long last_user_interaction = 0;
 
 unsigned long last_refresh = 0;
 
-volatile bool alarm_active = true;
-volatile bool change_alarm = false;
-bool flame;
-
 sensors_t all_sensors[10];
 int number_of_devices = 0;
 
 void connectToWiFi();
-void IRAM_ATTR alarmInterrupt();
 void IRAM_ATTR deviceDisplayInterrupt();
 void IRAM_ATTR isrInc();
-void IRAM_ATTR isrDec();
 void printDisplayInfo();
 void connectToMQTTBroker();
 void mqttMessageReceived(String &topic, String &payload);
@@ -85,14 +75,10 @@ void setup()
   byte error = Wire.endTransmission();
 
   // set BUTTON pin as input with pull-up
-  pinMode(ALARM_BUTTON, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(ALARM_BUTTON), alarmInterrupt, FALLING);
   pinMode(DEVICE_BUTTON, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(DEVICE_BUTTON), deviceDisplayInterrupt, FALLING);
   pinMode(incPin, INPUT_PULLUP);
-  pinMode(decPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(incPin), isrInc, FALLING);
-  attachInterrupt(digitalPinToInterrupt(decPin), isrDec, FALLING);
 
   pinMode(BUZZER, OUTPUT);
   digitalWrite(BUZZER, LOW);
@@ -181,11 +167,6 @@ void loop()
       mqttClient.disconnect();
     }
 
-    if (alarm_active && flame)
-      digitalWrite(BUZZER, HIGH);
-    else
-      digitalWrite(BUZZER, LOW);
-
     unsigned long now = millis();
     if (now - last_refresh > DISPLAY_REFRESH_RATE)
     {
@@ -222,47 +203,6 @@ void isrInc()
 
     last_interrupt_inc = now;
     printDisplayInfo();
-  }
-}
-
-void isrDec()
-{
-  unsigned long now = millis();
-  if (now - last_interrupt_dec > BUTTON_DEBOUNCE_DELAY)
-  {
-    if (displayMode == 0)
-      displayMode = DISPLAY_MODE_N - 1;
-    else
-      displayMode--;
-
-#ifdef DEBUG
-    Serial.printf("DisplayMode: %d \n", displayMode);
-#endif
-
-    last_interrupt_dec = now;
-    printDisplayInfo();
-  }
-}
-
-void IRAM_ATTR alarmInterrupt()
-{
-  unsigned long now = millis();
-  if (now - last_interrupt_alarm > BUTTON_DEBOUNCE_DELAY)
-  {
-    alarm_active = !alarm_active;
-    last_interrupt_alarm = now;
-    change_alarm = true;
-
-    lcd.home();
-    lcd.clear();
-    lcd.printf("Alarm state:");
-    lcd.setCursor(0, 1);
-    lcd.printf("%s", alarm_active ? "ON" : "OFF");
-
-    if (alarm_active && flame)
-      digitalWrite(BUZZER, HIGH);
-    else
-      digitalWrite(BUZZER, LOW);
   }
 }
 
@@ -492,14 +432,6 @@ void mqttMessageReceived(String &topic, String &payload)
     if (data_type == "flame")
     {
       all_sensors[index].flame = sensor_doc["value"].as<bool>();
-      if (sensor_doc["value"].as<bool>())
-      {
-        flame = true;
-      }
-      else
-      {
-        flame = false;
-      }
       return;
     }
     if (data_type == "light")
