ca888761b1c3166fcbd3b6435c672d687558c506
Federica Di Lauro <federicadilauro1998@gmail.com>
2022-06-15 11:26:27 +0200

    fix sensors platformio

---
+++ sensors/Sensors/src/main.cpp
@@ -0,0 +1,696 @@
+#include <Arduino.h>
+
+// Include sensors libraries
+// --------------
+// Include DHT 11 library
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+// Include WIFi Library
+#include <ESP8266WiFi.h>
+// Include JSON Library
+#include <ArduinoJson.h>
+// Include MQTT Library
+#include <MQTT.h>
+
+// Include SECRETs
+#include "secrets.h"
+
+// Init Mode
+#define DEBUG
+
+// Sensors
+// --------------
+// Buildi-In LEDs
+#define LED1 D0
+#define LED2 D4
+// Flame Detector
+#define FLAME D1
+#define FLAME_LOG_DELAY 60000
+// DHT11 - Temperature & Humidity Sensor
+#define DHT_PIN D2
+#define DHT_TYPE DHT11  // Sensor type: DHT 11
+#define DHT_DELAY 60000 // Needed delay for DHT sensors (Warning! Min = 2000)
+// Photoresistor
+#define PHOTORESISTOR A0            // photoresistor pin
+#define PHOTORESISTOR_THRESHOLD 900 // turn led on for light values lesser than this
+#define PHOTORESISTOR_LOG_DELAY 60000
+// WiFi signal
+#define RSSI_THRESHOLD -60 // WiFi signal strength threshold
+#define RSSI_LOG_DELAY 60000
+
+#define MQTT_TOPIC_SETUP "unishare/devices/setup"
+
+// Actuators
+//-----
+#define LIGHT D5
+#define AC_R D6
+#define AC_G D7
+#define AC_B D8
+
+#define AC_CONTROL_DELAY 60000
+
+// Init
+// --------------
+// Initialize millis var
+unsigned long currentTime;
+// Initialize setup time
+unsigned long lastSetupTime = 0;
+// Initialize temperature & humidity time
+unsigned long lastTempTime = 0;
+// Initialize photoresistor log time
+unsigned long lastLightLogTime = 0;
+// Initialize flame log time
+unsigned long lastFlameLogTime = 0;
+// Initialize rssi log time
+unsigned long lastRssiLog = 0;
+// Initialize ac control time
+unsigned long lastAcControl = 0;
+bool temp_read = false;
+
+// Initialize DHT sensor
+DHT dht = DHT(DHT_PIN, DHT_TYPE);
+
+// Configs
+// --------------
+// WiFi config
+char ssid[] = SECRET_SSID; // your network SSID (name)
+char pass[] = SECRET_PASS; // your network password
+#ifdef IP
+IPAddress ip(IP);
+IPAddress subnet(SUBNET);
+IPAddress dns(DNS);
+IPAddress gateway(GATEWAY);
+#endif
+// MQTT cfg
+#define MQTT_BUFFER_SIZE 1024            // the maximum size for packets being published and received
+MQTTClient mqttClient(MQTT_BUFFER_SIZE); // handles the MQTT communication protocol
+WiFiClient networkClient;                // handles the network connection to the MQTT broker
+
+String clean_mac_address;
+String sensors_topic = "unishare/sensors/";
+String control_topic = "unishare/control/";
+String light_control_topic;
+String ac_control_topic;
+String mqtt_topic_status = "unishare/devices/status/";
+
+// Globals
+// --------------
+// Sensors data values
+bool data_light;
+bool data_flame;
+double data_temperature;
+double data_apparent_temperature;
+double data_humidity;
+
+// actuators values;
+double ac_temp;
+String ac_mode;
+String ac_previous_state;
+
+// Functions
+// -------------------------------
+void printWifiStatus();
+
+long connectToWiFi();
+
+void connectToMQTTBroker();
+
+void mqttMessageReceived(String &topic, String &payload);
+
+String clearMacAddress(String mac_address);
+
+void sendMqttDouble(String attribute, double value);
+
+void sendMqttLong(String attribute, long value);
+
+void sendMqttBool(String attribute, bool value);
+
+void acAutoControl();
+
+unsigned long setup_time;
+bool first_loop = true;
+
+// CODE
+void setup()
+{
+  setup_time = millis();
+  // Sync Serial logs
+  Serial.begin(115200);
+
+  // Init PINs
+  pinMode(LED1, OUTPUT); // Define LED 1 output pin
+  pinMode(LED2, OUTPUT); // Define LED 2 output pin
+  pinMode(FLAME, INPUT); // Define FLAME input pin
+
+  // Init actuators
+  pinMode(LIGHT, OUTPUT);
+  pinMode(AC_R, OUTPUT);
+  pinMode(AC_G, OUTPUT);
+  pinMode(AC_B, OUTPUT);
+
+  // Turn actuators off
+  digitalWrite(LIGHT, LOW);
+  digitalWrite(AC_R, LOW);
+  digitalWrite(AC_G, LOW);
+  digitalWrite(AC_B, LOW);
+
+  // Turn LEDs OFF
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+
+  // Start DHT
+  dht.begin();
+
+  // Start MQTT
+  mqttClient.begin(MQTT_BROKERIP, 1883, networkClient); // setup communication with MQTT broker
+  mqttClient.onMessage(mqttMessageReceived);            // callback on message received from MQTT broker
+
+  // Start WiFi
+  WiFi.mode(WIFI_STA);
+
+  clean_mac_address = clearMacAddress(String(WiFi.macAddress()));
+  mqtt_topic_status = mqtt_topic_status + clean_mac_address;
+
+  DynamicJsonDocument doc_will(128);
+  doc_will["connected"] = false;
+  char buffer_will[128];
+  serializeJson(doc_will, buffer_will);
+  const char *topic_status = mqtt_topic_status.c_str();
+  mqttClient.setWill(topic_status, buffer_will, true, 1);
+
+#ifdef DEBUG
+  Serial.println("Setup completed!");
+#endif
+
+  // Init delay
+  delay(2000);
+}
+
+bool sent_setup = false;
+void loop()
+{
+  if (!sent_setup)
+  {
+    connectToWiFi(); // connect to WiFi (if not already connected)
+    light_control_topic = control_topic + clean_mac_address + "/light";
+    ac_control_topic = control_topic + clean_mac_address + "/ac";
+    connectToMQTTBroker(); // connect to MQTT broker (if not already connected)
+    DynamicJsonDocument doc(256);
+    doc["mac_address"] = clean_mac_address;
+    doc["type"] = "sensors";
+    doc["name"] = "sensors1";
+    char buffer[256];
+    size_t n = serializeJson(doc, buffer);
+
+#ifdef DEBUG
+    Serial.print(F("JSON setup message: "));
+    Serial.println(buffer);
+#endif
+    if (mqttClient.publish(MQTT_TOPIC_SETUP, buffer, n, false, 1))
+      sent_setup = true;
+  }
+  else
+  {
+    long rssi = connectToWiFi(); // connect to WiFi (if not already connected)
+    connectToMQTTBroker();       // connect to MQTT broker (if not already connected)
+
+    if (!mqttClient.loop())
+    {
+#ifdef DEBUG
+      Serial.println(mqttClient.lastError());
+#endif
+      mqttClient.disconnect();
+    }
+
+    String attribute = "";
+    // Get millis time
+    currentTime = millis();
+    // // Send to MQTT
+    // Check if frequency is good :)
+    if (currentTime - lastRssiLog > RSSI_LOG_DELAY)
+    {
+      // Update reading time
+      lastRssiLog = currentTime;
+      // Send to MQTT
+      attribute = "rssi";
+      sendMqttLong(attribute, rssi);
+    }
+    // PHOTORESISTOR
+    // -----------------------------
+    unsigned int lightSensorValue;
+    currentTime = millis();
+    // Re-send LIGHT data sometimes :)
+    if (currentTime - lastLightLogTime > PHOTORESISTOR_LOG_DELAY)
+    {
+      // Update reading time
+      lastLightLogTime = currentTime;
+      lightSensorValue = analogRead(PHOTORESISTOR); // read analog value (range 0-1023)
+      if (lightSensorValue >= PHOTORESISTOR_THRESHOLD)
+      { // high brightness
+        // Set the LED off
+        digitalWrite(LED1, HIGH);
+        // Update if status is changed
+        // if (data_light == false) sendBooleanDataToMQTT("light", true);
+        // Set status on memory
+        data_light = true;
+      }
+      else
+      { // low brightness
+        // Set the LED on
+        digitalWrite(LED1, LOW);
+        // Update if status is changed
+        // if (data_light == true) sendBooleanDataToMQTT("light", false);
+        // Set status on memory
+        data_light = false;
+      }
+
+      // Send data to MQTT
+      attribute = "light";
+      sendMqttBool(attribute, data_light);
+    }
+
+    // FLAME DETECTION
+    // -----------------------------
+    // Read digital PIN of Flame Sensor
+    int fire = digitalRead(FLAME); // Read FLAME sensor
+    // Check flame level
+    if (fire == HIGH && !data_flame)
+    {
+
+#ifdef DEBUG
+      Serial.println("Fire! Fire!");
+#endif
+
+      // Save flame data
+      data_flame = true;
+
+      // Send data to MQTT
+      attribute = "flame";
+      sendMqttBool(attribute, data_flame);
+    }
+    else if (fire == LOW && data_flame)
+    {
+
+#ifdef DEBUG
+      Serial.println("No more fire!");
+#endif
+      data_flame = false;
+
+      attribute = "flame";
+      sendMqttBool(attribute, data_flame);
+    }
+    currentTime = millis();
+    // Re-send FLAME data sometimes :)
+    if (currentTime - lastFlameLogTime > FLAME_LOG_DELAY)
+    {
+
+      // Update reading time
+      lastFlameLogTime = currentTime;
+
+      attribute = "flame";
+      sendMqttBool(attribute, data_flame);
+    }
+
+    // TEMPERATURE & HUMIDITY DETECTION
+    // -------------------------------
+    currentTime = millis();
+    temp_read = false;
+    // Check if frequency is good :)
+    if (currentTime - lastTempTime > DHT_DELAY)
+    {
+
+      // Update reading time
+      lastTempTime = currentTime;
+
+      // reading temperature or humidity takes about 250 milliseconds!
+      double h = dht.readHumidity();    // humidity percentage, range 20-80% (±5% accuracy)
+      double t = dht.readTemperature(); // temperature Celsius, range 0-50°C (±2°C accuracy)
+
+      if (isnan(h) || isnan(t))
+      { // readings failed, skip
+        Serial.println(F("Failed to read from DHT sensor!"));
+        return;
+      }
+
+      temp_read = true;
+      // compute heat index in Celsius (isFahreheit = false)
+      double hic = dht.computeHeatIndex(t, h, false);
+
+      data_humidity = h;
+      data_temperature = t;
+      data_apparent_temperature = hic;
+
+#ifdef DEBUG
+      Serial.print(F("Humidity: "));
+      Serial.print(h);
+      Serial.print(F("%  Temperature: "));
+      Serial.print(t);
+      Serial.print(F("°C  Apparent temperature: ")); // the temperature perceived by humans (takes into account humidity)
+      Serial.print(hic);
+      Serial.println(F("°C"));
+#endif
+
+      attribute = "humidity";
+      sendMqttDouble(attribute, data_humidity);
+      attribute = "temperature";
+      sendMqttDouble(attribute, data_temperature);
+      attribute = "apparent_temperature";
+      sendMqttDouble(attribute, data_apparent_temperature);
+    }
+
+    // automatic AC control
+    if (ac_mode == "auto" && (currentTime - lastAcControl > AC_CONTROL_DELAY))
+    {
+      if (!temp_read)
+      {
+        double t = dht.readTemperature(); // temperature Celsius, range 0-50°C (±2°C accuracy)
+
+        if (isnan(t))
+        { // readings failed, skip
+          Serial.println(F("Failed to read from DHT sensor!"));
+          return;
+        }
+        data_temperature = t;
+      }
+      acAutoControl();
+    }
+  }
+  if (first_loop){
+    unsigned long now_time_setup = millis();
+    setup_time = now_time_setup - setup_time;
+    first_loop = false;
+  }
+  Serial.println(setup_time);
+}
+
+// Functions
+// -------------------------------
+void printWifiStatus()
+{
+  Serial.println(F("\n=== WiFi connection status ==="));
+
+  // SSID
+  Serial.print(F("SSID: "));
+  Serial.println(WiFi.SSID());
+
+  // signal strength
+  Serial.print(F("Signal strength (RSSI): "));
+  Serial.print(WiFi.RSSI());
+  Serial.println(F(" dBm"));
+
+  // current IP
+  Serial.print(F("IP Address: "));
+  Serial.println(WiFi.localIP());
+
+  // subnet mask
+  Serial.print(F("Subnet mask: "));
+  Serial.println(WiFi.subnetMask());
+
+  // gateway
+  Serial.print(F("Gateway IP: "));
+  Serial.println(WiFi.gatewayIP());
+
+  // DNS
+  Serial.print(F("DNS IP: "));
+  Serial.println(WiFi.dnsIP());
+
+  Serial.println(F("==============================\n"));
+}
+
+long connectToWiFi()
+{
+  long rssi_strength;
+  // connect to WiFi (if not already connected)
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(F("Connecting to SSID: "));
+    Serial.println(ssid);
+
+#ifdef IP
+    WiFi.config(ip, dns, gateway, subnet); // by default network is configured using DHCP
+#endif
+
+#ifdef DEBUG
+    Serial.print(F("Connecting"));
+#endif
+    WiFi.begin(ssid, pass);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+#ifdef DEBUG
+      Serial.print(F("."));
+#endif
+      delay(150);
+    }
+#ifdef DEBUG
+    Serial.println(F("\nConnected!"));
+#endif
+
+    rssi_strength = WiFi.RSSI(); // get wifi signal strength
+
+#ifdef DEBUG
+    printWifiStatus();
+#endif
+  }
+  else
+  {
+    rssi_strength = WiFi.RSSI(); // get wifi signal strength
+  }
+
+  return rssi_strength;
+}
+
+void connectToMQTTBroker()
+{
+  if (!mqttClient.connected())
+  { // not connected
+
+#ifdef DEBUG
+    Serial.print(F("\nConnecting to MQTT broker..."));
+#endif
+
+    while (!mqttClient.connect(MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD))
+    {
+      Serial.print(F("."));
+      delay(150);
+    }
+
+#ifdef DEBUG
+    Serial.println(F("\nConnected!"));
+#endif
+
+    mqttClient.subscribe(light_control_topic, 1);
+    mqttClient.subscribe(ac_control_topic, 1);
+#ifdef DEBUG
+    Serial.println("Subscribed to " + light_control_topic + "topic");
+    Serial.println("Subscribed to " + ac_control_topic + "topic");
+#endif
+
+    DynamicJsonDocument doc_stat(128);
+    doc_stat["connected"] = true;
+    char buffer_stat[128];
+    size_t n = serializeJson(doc_stat, buffer_stat);
+    const char *topic_status = mqtt_topic_status.c_str();
+    mqttClient.publish(topic_status, buffer_stat, n, true, 1);
+  }
+}
+
+void mqttMessageReceived(String &topic, String &payload)
+{
+// This function handles a message from the MQTT broker
+#ifdef DEBUG
+  Serial.println("Incoming MQTT message: " + topic + " - " + payload);
+#endif
+  if (topic == light_control_topic)
+  {
+
+    StaticJsonDocument<128> doc;
+    deserializeJson(doc, payload);
+    String light_control = doc["control"];
+
+    if (light_control == "on")
+    {
+      digitalWrite(LIGHT, HIGH);
+#ifdef DEBUG
+      Serial.println("Light on");
+#endif
+      return;
+    }
+    else if (light_control == "off")
+    {
+      digitalWrite(LIGHT, LOW);
+#ifdef DEBUG
+      Serial.println("Light off");
+#endif
+      return;
+    }
+    else
+    {
+#ifdef DEBUG
+      Serial.println("Unrecognized light command!");
+#endif
+      return;
+    }
+  }
+  if (topic == ac_control_topic)
+  {
+    StaticJsonDocument<256> doc;
+    deserializeJson(doc, payload);
+    ac_mode = doc["control"].as<String>();
+    ;
+    if (ac_mode == "on")
+    {
+      digitalWrite(AC_R, LOW);
+      digitalWrite(AC_G, HIGH);
+      digitalWrite(AC_B, LOW);
+#ifdef DEBUG
+      Serial.println("AC on");
+#endif
+      return;
+    }
+    else if (ac_mode == "off")
+    {
+      digitalWrite(AC_R, HIGH);
+      digitalWrite(AC_G, LOW);
+      digitalWrite(AC_B, LOW);
+#ifdef DEBUG
+      Serial.println("AC off");
+#endif
+      return;
+    }
+    else if (ac_mode == "auto")
+    {
+      ac_temp = doc["temp"];
+#ifdef DEBUG
+      Serial.println("AC auto");
+      Serial.printf("Actual temperature: %f \n", data_temperature);
+      Serial.printf("Desired temperature: %f \n", ac_temp);
+#endif
+      return;
+    }
+    else
+    {
+#ifdef DEBUG
+      Serial.println("Unrecognized AC command");
+#endif
+      return;
+    }
+  }
+  return;
+}
+
+String clearMacAddress(String mac_address)
+{
+  // Prepare
+  String to_replace = String(':');
+  String replaced = "";
+  // Exec
+  mac_address.replace(to_replace, replaced);
+  // Return
+  return mac_address;
+}
+
+void sendMqttDouble(String attribute, double value)
+{
+  // Send data to MQTT
+  DynamicJsonDocument doc(128);
+  doc["value"] = value;
+  char buffer[128];
+  size_t n = serializeJson(doc, buffer);
+  String topic = sensors_topic + clean_mac_address + "/" + attribute;
+  const char *topic_c = topic.c_str();
+  bool sent = false;
+  if (mqttClient.publish(topic_c, buffer, n, true, 1))
+    sent = true;
+#ifdef DEBUG
+  Serial.println(topic);
+  Serial.print(F("JSON message: "));
+  Serial.println(buffer);
+  if (sent)
+    Serial.println("Send OK");
+  else
+    Serial.println("Send NOT OK");
+#endif
+}
+
+void sendMqttLong(String attribute, long value)
+{
+  // Send data to MQTT
+  DynamicJsonDocument doc(128);
+  doc["value"] = value;
+  char buffer[128];
+  size_t n = serializeJson(doc, buffer);
+  String topic = sensors_topic + clean_mac_address + "/" + attribute;
+  const char *topic_c = topic.c_str();
+  bool sent = false;
+  if (mqttClient.publish(topic_c, buffer, n, true, 1))
+    sent = true;
+#ifdef DEBUG
+  Serial.println(topic);
+  Serial.print(F("JSON message: "));
+  Serial.println(buffer);
+  if (sent)
+    Serial.println("Send OK");
+  else
+    Serial.println("Send NOT OK");
+#endif
+}
+
+void sendMqttBool(String attribute, bool value)
+{
+  // Send data to MQTT
+  DynamicJsonDocument doc(128);
+  doc["value"] = value;
+  char buffer[128];
+  size_t n = serializeJson(doc, buffer);
+  String topic = sensors_topic + clean_mac_address + "/" + attribute;
+  const char *topic_c = topic.c_str();
+  bool sent = false;
+  if (mqttClient.publish(topic_c, buffer, n, true, 1))
+    sent = true;
+#ifdef DEBUG
+  Serial.println(topic);
+  Serial.print(F("JSON message: "));
+  Serial.println(buffer);
+  if (sent)
+    Serial.println("Send OK");
+  else
+    Serial.println("Send NOT OK");
+#endif
+}
+
+void acAutoControl()
+{
+  String ac_current_state;
+  if (data_temperature >= ac_temp)
+  {
+    ac_current_state = "on";
+  }
+  else
+  {
+    ac_current_state = "off";
+  }
+
+  if (ac_current_state != ac_previous_state)
+  {
+    ac_previous_state = ac_current_state;
+
+    if (ac_current_state == "on")
+    {
+      digitalWrite(AC_R, LOW);
+      digitalWrite(AC_G, LOW);
+      digitalWrite(AC_B, HIGH);
+#ifdef DEBUG
+      Serial.println("High temp, turn AC on");
+#endif
+    }
+    else
+    {
+      digitalWrite(AC_R, HIGH);
+      digitalWrite(AC_G, LOW);
+      digitalWrite(AC_B, HIGH);
+#ifdef DEBUG
+      Serial.println("Low temp, turn AC off");
+#endif
+    }
+  }
+}
