c1ac1e8b32ade6b443bedcf1d4cb442449d11beb
Gabriele De Rosa <derosagabriele@outlook.it>
2022-06-20 02:04:19 +0200

    Add footer

--- gui/src/components/Footers/FooterAdmin.js
+++ gui/src/components/Footers/FooterAdmin.js
@@ -9,28 +9,20 @@ export default function FooterAdmin() {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4">
               <div className="text-sm text-blueGray-500 font-semibold py-1 text-center md:text-left">
-                Copyright © {new Date().getFullYear()}{" "}
+                Copyright © {new Date().getFullYear()}{" - "}
                 <a
-                  href="https://www.creative-tim.com?ref=nr-footer-admin"
+                  href="/about"
                   className="text-blueGray-500 hover:text-blueGray-700 text-sm font-semibold py-1"
                 >
-                  Unishare
+                  Unishare Team
                 </a>
               </div>
             </div>
-            <div className="display-none w-full md:w-8/12 px-4">
+            <div className="w-full md:w-8/12 px-4">
               <ul className="flex flex-wrap list-none md:justify-end  justify-center">
                 <li>
                   <a
-                    href="https://www.creative-tim.com?ref=nr-footer-admin"
-                    className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
-                  >
-                    Unishare
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.creative-tim.com/presentation?ref=nr-footer-admin"
+                    href="/about"
                     className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
                   >
                     About Us
@@ -38,15 +30,7 @@ export default function FooterAdmin() {
                 </li>
                 <li>
                   <a
-                    href="http://blog.creative-tim.com?ref=nr-footer-admin"
-                    className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
-                  >
-                    Blog
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://github.com/creativetimofficial/notus-react/blob/main/LICENSE.md?ref=nr-footer-admin"
+                    href="https://github.com/derogab/home-monitor/blob/master/LICENSE"
                     className="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
                   >
                     MIT License
